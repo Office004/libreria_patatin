40d933e38dcdc604b00d315a0c54fc6ff7589185
Vito Colucci <vito.colucci@finconsgroup.com>
2025-05-14 16:16:45 +0200

    Primo commit: libreria_patapim

---
+++ src/main/java/com/patatin/libreria_da_patatin/LibreriaDaPatatinApplication.java
@@ -0,0 +1,13 @@
+package com.patatin.libreria_da_patatin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaDaPatatinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaDaPatatinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patatin/libreria_da_patatin/LibreriaDaPatatinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patatin.libreria_da_patatin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaDaPatatinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
